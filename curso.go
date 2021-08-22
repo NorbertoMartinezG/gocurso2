229f3f0721a69a2cdf1ede91940a143b0055e700
Drack <nautilus_com@yahoo.com>
2021-08-22 04:47:06 -0500

    Curso Go

---
+++ curso.go
@@ -0,0 +1,11 @@
+// configuracion de acuerdo al curso :
+// https://youtube.com/playlist?list=PLt1J5u9LpM5-L-Ps8jjr91pKhFxAnxKJp
+// https://www.youtube.com/watch?v=FeDZKgB13Qc&list=PLt1J5u9LpM5-L-Ps8jjr91pKhFxAnxKJp&index=1&t=1s
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hola")
+}
