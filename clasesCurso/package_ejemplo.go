88cb5871bcb53701fdfa195df6e992370e05fc20
Drack <nautilus_com@yahoo.com>
2021-08-24 06:53:45 -0500

    clase 1_2 estructura basica

--- clasesCurso/package_ejemplo.go
+++ clasesCurso/package_ejemplo.go
@@ -4,10 +4,10 @@ import "fmt"
 
 //funcionPrivadaIniciaConMinuscula funcion privada se inicia con letra minuscula
 func funcionPrivadaIniciaConMinuscula() {
-
+	fmt.Println("FUNCION PRIVADA EMPIEZA CON MINUSCULA")
 }
 
 //FuncionPublicaIniciaConMayuscula funcion privada se inicia con letra mayuscula
 func FuncionPublicaIniciaConMayuscula() {
-	fmt.Println("FUNCION PUBLICA")
+	fmt.Println("FUNCION PUBLICA EMPIEZA CON MAYUSCULA")
 }
--- curso.go
+++ curso.go
@@ -1,16 +1,16 @@
 package main
 
+// clase 2
 import (
-	"fmt"
+	"fmt" //paquete format
 
 	"github.com/NorbertoMG/gocurso2/clasesCurso"
 	// HAY QUE USAR EL SIG COMANDO EN TERMINAL PARA QUE DETECTE PAQUETES "go env -w GO111MODULE=auto"
 )
 
 func main() {
-	fmt.Println("hola")
-	fmt.Println("hola 33")
 
+	fmt.Println("Curso Go")
 	clasesCurso.FuncionPublicaIniciaConMayuscula()
 
 }
